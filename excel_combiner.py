11f6029cd3634501e769a4626bcc4e5391ad8104
Gabriel Bac <gabibac8@gmail.com>
2024-02-10 12:51:51 +0000

    added excel_combiner.py

---
+++ excel_combiner.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+ROOT_DIR = os.path.abspath("")
+EXCELS_DIR = os.path.join(ROOT_DIR, "auction_results")
+
+files = next(os.walk(EXCELS_DIR))[2]
+files_dir = [os.path.join(EXCELS_DIR, f) for f in files]
+
+df = pd.concat([pd.read_excel(f) for f in files_dir], ignore_index=True)
+
+df.to_excel(os.path.join((EXCELS_DIR), "combined.xlsx"))
